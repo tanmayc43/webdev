5a0fe734bb8f30955efe8221e8b21f586bf6cf52
Tanmay Chaturvedi <168345467+tanmayc43@users.noreply.github.com>
2024-12-06 12:50:59 +0000

    dataTypes added

---
+++ js/01_basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") //-> this syntax not available in nodeJS
+
+//maintain code readability
+
+let name = "tanmay"
+let age = 20
+let isLockedIn = true
+
+//number => 2^53
+// bigint
+// string " "
+// boolean
+// null => standalone value
+// undefined => no value defined, different from null
+// symbol => uniqueness teller
+
+// object
+
+console.log(typeof(null))
+
+// null's type is object
+// undefined's type is undefined
+
