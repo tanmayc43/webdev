4ea96a838933ecacd4ebcc15fd9bc6219a10abdf
Tanmay Chaturvedi <168345467+tanmayc43@users.noreply.github.com>
2024-12-06 12:34:13 +0000

    variables in js

---
+++ js/01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 123323
+let accountEmail = "something@company.com"
+var accountPassword = "secret123"
+accountCity = "Delhi"
+
+let accountState
+//accountId = 124424
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+/*
+Never use VAR
+(because of issue in blockscope)
+
+Penalti a Favor del Real VARdrid
+*/
+
